701fd954bac89307e5ffaa58618896e464ed1d2c
shintocv <cshintov@gmail.com>
2015-09-02 16:33:58 +0530

    modifying python_vm

--- c_projects/python_vm/testcases/factorial.py
+++
@@ -1,9 +0,0 @@
-# computes factorial(num)
-num = 5
-print num
-fact = 1
-while num > 0:
-    fact = fact * num
-    num = num - 1
-
-print fact
--- c_projects/python_vm/testcases/parity.py
+++
@@ -1,7 +0,0 @@
-#checks parity of num ; result 1 means odd and 0 means even
-num = 8 
-print num
-if num % 2 == 0:
-    print 0
-else:
-    print 1
